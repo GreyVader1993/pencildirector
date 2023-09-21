17aa5af09396404de2d9e65749f26f8a2df51906
Tanmay Dureja <tanmaydureja@gmail.com>
2023-09-20 21:23:24 -0700

    Test commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import HomepageMasthead from '../components/HomepageMasthead/index.jsx';
+// import HomepageScroll from '../components/HomepageScroll/index.jsx';
+
 const Home = () => {
     return (
         <>
             <HomepageMasthead />
+            {/* <HomepageScroll /> */}
         </>
     );
 }
