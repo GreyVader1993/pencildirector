ba1158e88ad522845e9421fbde463982306c6397
Tanmay Dureja <tanmaydureja@gmail.com>
2024-03-18 18:59:39 -0700

    Test

--- src/components/HomepageTimeline/index.jsx
+++ src/components/HomepageTimeline/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomepageTimeline = () => {
   return (
-    <div class="bg-amber-50 mx-auto max-w-7xl space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+    <div class="bg-amber-50  mx-auto max-w-7xl space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
       <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
         <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
           <svg
